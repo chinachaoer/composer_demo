09c1dd7e0cdffd3775583b897148c018d9f6e739
santong <santong@51huxin.com>
2017-06-29 13:57:35 +0800

    add

---
+++ Controller/Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by 阳光互信.
+ * User: santong@51huxin.com
+ * Date: 2017/6/29
+ * Time: 13:40
+ */
+
+namespace Controller;
+class Controller{
+    public static function run(){
+        echo '我的composer demo';
+    }
+}
---
+++ web/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by 阳光互信.
+ * User: santong@51huxin.com
+ * Date: 2017/6/29
+ * Time: 13:42
+ */
+include '../vendor/autoload.php';
+Controller\Conrtoller::run();
